e366793dfea7e9d6816e096bec7516a61086d4d4
Philippe Bernard <philippe.bernard@gmail.com>
2021-09-20 13:41:50 +0200

    Call Chromium

--- src/netlify-function.js
+++ src/netlify-function.js
@@ -1,3 +1,19 @@
+const chromium = require('chrome-aws-lambda');
+
 exports.handler = async (event, context) => {
-  return { statusCode: 200, body: "Hello from Resoc!" };
+  try {
+    const browser = await chromium.puppeteer.launch({
+      executablePath: await chromium.executablePath,
+      args: chromium.args,
+      headless: chromium.headless
+    });
+
+    return { statusCode: 200, body: "Hello from Resoc!" };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error :(' }),
+    };
+  }
 };
